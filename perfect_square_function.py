8e42f813e60e3607ea5073ec80f75eaf49ea6348
KyleBeyers <kylebey93@gmail.com>
2016-11-28 13:27:33 +0000

    Week 2

--- perfect_square_function.py
+++ perfect_square_function.py
@@ -1,6 +1,24 @@
 ##210CT LabWeek 2
 ##Task 1
 
+##PERFECT_SQUARE(N):
+##    x <- 0
+##    if n > 0 then:
+##        while x * x < n:
+##            x <- x + 1
+##        if x * x != n then:
+##            PERFECT_SQUARE(N-1)
+##        if x * x = n then:
+##            if n = value then:
+##                output n is a perfect square
+##            if n < value then:
+##                output closest perfect square is n
+##    else:
+##        output value is not a positive integer
+##
+##value <- user input converted to type int
+##PERFECT_SQUARE(value)
+
 def perfectSquare(n):
     """Function to check if a value is a perfect square, or find the closest
     perfect square value to input"""
